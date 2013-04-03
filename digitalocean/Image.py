c69b2fa4b23c64edf96d09e67a8863847c9e4bdf
John Engdal <john@engdal.fi>
2013-04-03 13:49:20 +0300

    Image now supports the destroy api call.

--- digitalocean/Image.py
+++ digitalocean/Image.py
@@ -8,3 +8,19 @@ class Image(object):
         self.name = None
         self.id = None
         self.distribution = None
+
+    def __call_api(self, path, params=None):
+        payload = {'client_id': self.client_id, 'api_key': self.api_key}
+        if params:
+            payload.update(params)
+        r = requests.get("https://api.digitalocean.com/images/%s%s" % (self.id, path), params=payload)
+        data = r.json()
+        if data['status'] != "OK":
+            return None # Raise?
+        return data
+
+    def destroy(self):
+        """
+            Restore the droplet to an image ( snapshot or backup )
+        """
+        self.__call_api("/destroy/")
